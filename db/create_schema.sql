eff9c977f5fff56ac469d6e9c60994b4e5147465
Tomas Skalicky <skalicky.tomas@gmail.com>
2011-11-25 18:07:21 +0100

    test

--- db/create_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-	id INTEGER NOT NULL PRIMARY KEY,
-	description VARCHAR(100) NOT NULL,
-	price DECIMAL(15, 2) NOT NULL
-);
-
-CREATE INDEX products_description ON products(description);
